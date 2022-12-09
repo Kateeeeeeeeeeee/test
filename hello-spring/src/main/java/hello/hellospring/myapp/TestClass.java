b76b6393d80d3a5e8219f9013cc80819c31ec931
최혜민 <hmmh112@naver.com>
2022-12-09 14:50:51 +0900

    TestClass 메소드 main수정

--- hello-spring/src/main/java/hello/hellospring/myapp/TestClass.java
+++ hello-spring/src/main/java/hello/hellospring/myapp/TestClass.java
@@ -1,12 +1,13 @@
 package hello.hellospring.myapp;
 
 public class TestClass {
-	private void sout() {
+	private void mian() {
 		System.out.println("Hello");
 
 				//수정 
 		System.out.println("Hi");
 		// 혜민 수정
+		
 	}
 
 }
